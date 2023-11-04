9f9d7400c1894efc57a18bf1daada97bf72c8e47
Demunter Bao <bao.demunter@gmail.com>
2023-11-04 15:45:27 +0100

    refact: extract Content of Card to make Card shared

---
+++ src/Components/Molecules/Card/Card.tsx
@@ -0,0 +1,17 @@
+﻿import { ReactNode } from 'react';
+
+interface Props {
+	children: ReactNode;
+	functionOnClick?: () => void;
+}
+
+const Card = ({ children, functionOnClick }: Readonly<Props>) => {
+	return (
+		<button onClick={() => functionOnClick?.()}
+				className="flex flex-col relative h-32 w-64 bg-white overflow-hidden rounded-xl py-3 px-6 shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none transition-shadow duration-300">
+			{children}
+		</button>
+	);
+};
+
+export default Card;
---
+++ src/Components/Molecules/Card/CardContentComponent/DisplayCard/DisplayCard.tsx
@@ -0,0 +1,48 @@
+﻿import { getUniqueID } from '../../../../../Helpers/UniqueId.Helper.ts';
+import IconWithText from '../../../../Atoms/IconWithText/IconWithText.tsx';
+
+interface ITextWithIcon {
+	text: string;
+	icon: string;
+}
+
+interface Props {
+	title?: string;
+	topRightText?: ITextWithIcon | string;
+	informations?: ITextWithIcon[];
+	bottomRightCard?: ITextWithIcon;
+	functionOnClick?: () => void;
+}
+
+const DisplayCard = ({ title, topRightText, informations, bottomRightCard }: Readonly<Props>) => {
+	return (
+		<>
+			<div className="flex justify-between w-full">
+				<h3 className="pl-1 font-semibold text-gray-700">{title}</h3>
+				{topRightText &&
+					<p className="text-gray-500">
+						{typeof topRightText == 'string'
+							? topRightText
+							: <IconWithText icon={topRightText.icon} text={topRightText.text}/>
+						}
+					</p>
+				}
+			</div>
+			<div className="flex flex-col-reverse gap-1 h-full w-full">
+				{informations?.map((info, index) => (
+					<IconWithText key={getUniqueID(info.text, index)} icon={info.icon} text={info.text}/>
+				))}
+			</div>
+			
+			<div className="absolute bottom-0 right-0 py-2 px-4 rounded-tl-xl border-t border-l border-gray-100 shadow-md bg-white">
+				<p>{bottomRightCard?.text}
+					<span className="text-xl">
+						{bottomRightCard?.icon}
+					</span>
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default DisplayCard;
