68c2385b646b638738b6f436a85753371a8cf4fa
Demunter Bao <bao.demunter@gmail.com>
2023-11-04 03:09:06 +0100

    add: props to card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import Card from './Components/Molecules/Card.tsx';
 
-function App() {
+const roomInformation = [
+	{ text: '100%', icon: '😊' },
+	{ text: 'Exposition', icon: '☀️️' },
+];
 
-  return (
-    <div className="w-screen h-screen bg-gray-100 p-10">
-        <Card title="Salon" topRightText="22°C"/>
-    </div>
-  )
+const plantsQuantity = { text: '3', icon: '🪴' };
+
+function App () {
+	
+	return (
+		<div className="w-screen h-screen bg-gray-100 p-10">
+			<Card title="Salon"
+				  topRightText="22°C"
+				  informations={roomInformation}
+				  bottomRightCard={plantsQuantity}
+				  functionOnClick={() => console.log('click')}
+			/>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/Components/Molecules/Card.tsx
+++ src/Components/Molecules/Card.tsx
@@ -1,20 +1,39 @@
-﻿interface Props {
-	title?: string;
-	topRightText?: string;
+﻿import { getUniqueID } from '../../Helpers/UniqueId.Helper.ts';
+
+interface ITextWithIcon {
+	text: string;
+	icon: string;
 }
 
-const Card = ({ title, topRightText }: Readonly<Props>) => {
+interface Props {
+	title?: string;
+	topRightText?: string;
+	informations?: ITextWithIcon[];
+	bottomRightCard?: ITextWithIcon;
+	functionOnClick?: () => void;
+}
+
+const Card = ({ title, topRightText, informations, bottomRightCard, functionOnClick }: Readonly<Props>) => {
 	return (
-		<div className="flex flex-col relative h-32 w-64 bg-white overflow-hidden rounded-xl py-3 px-6 shadow-md">
-			<div className="flex justify-between">
+		<button onClick={() => functionOnClick?.()}
+			className="flex flex-col relative h-32 w-64 bg-white overflow-hidden rounded-xl py-3 px-6 shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none transition-shadow duration-300">
+			<div className="flex justify-between w-full">
 				<h3 className="font-semibold text-gray-700">{title}</h3>
 				<p className="text-gray-500">{topRightText}</p>
 			</div>
+			<div className="flex flex-col-reverse gap-1 h-full w-full">
+				{informations?.map((info, index) => (
+					<div key={getUniqueID(info.text, index)} className="flex">
+						<p className="text-gray-500">{info.icon}</p>
+						<p className="text-gray-500">{info.text}</p>
+					</div>
+				))}
+			</div>
 			
-			<div className="absolute bottom-0 right-0 py-3 px-6 rounded-tl-xl border-t border-l
-				border-gray-100 transition-shadow shadow-md hover:shadow-inner
-				cursor-pointer duration-500 bg-white"><p>7🪴</p></div>
-		</div>
+			<div className="absolute bottom-0 right-0 py-2 px-4 rounded-tl-xl border-t border-l
+				border-gray-100
+				cursor-pointer duration-500 bg-white"><p>{bottomRightCard?.text} <span className="text-xl">{bottomRightCard?.icon}</span></p></div>
+		</button>
 	);
 };
 
