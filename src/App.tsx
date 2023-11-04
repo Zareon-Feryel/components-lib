c5a30e66525169f836ce2be9eb00c933f7ed27b4
Demunter Bao <bao.demunter@gmail.com>
2023-11-04 01:29:15 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Card from './Components/Molecules/Card.tsx';
+
+function App() {
+
+  return (
+    <div className="w-screen h-screen bg-gray-100 p-10">
+        <Card title="Salon" topRightText="22°C"/>
+    </div>
+  )
+}
+
+export default App
